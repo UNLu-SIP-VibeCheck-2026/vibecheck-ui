4a587c5db0cbead5bcf96d4c33178357c01f8dfe
MattZander24 <mattzanderm24mail.com>
2026-04-16 17:01:49 -0300

    Perfil de Usuario Namefix

--- src/app/components/shared/dialogs/change-pass-dialog/change-pass-dialog.component.ts
+++ src/app/components/shared/dialogs/change-pass-dialog/change-pass-dialog.component.ts
@@ -7,7 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-change-password-dialog',
+  selector: 'app-change-pass-dialog',
   standalone: true,
   imports: [
     CommonModule,
