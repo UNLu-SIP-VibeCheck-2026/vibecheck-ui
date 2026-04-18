04ca807f17fb23b507d4523f21e52a4250e36ff7
FrancoSaracho64 <fmsaracho64@gmail.com>
2026-04-18 02:01:38 -0300

    Ajustes de UX

--- src/app/components/shared/footer/footer.component.ts
+++ src/app/components/shared/footer/footer.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-footer',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  authService = inject(AuthService);
+  user$ = this.authService.currentUser$;
 }
