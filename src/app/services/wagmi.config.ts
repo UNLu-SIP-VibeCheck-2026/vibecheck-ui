55dd2821b43a05970d158853227037d4f52fe1eb
Gustavo Contardi <lito.contardi@hotmail.com>
2026-06-10 23:54:13 -0300

    soy argentino me sobran los huevos

--- src/app/services/wagmi.config.ts
+++ src/app/services/wagmi.config.ts
@@ -1,11 +1,15 @@
 import { createConfig, http } from '@wagmi/core';
 import { sepolia } from '@wagmi/core/chains';
-import { injected } from '@wagmi/core';
+import { walletConnect } from '@wagmi/core/connectors';
+import { environment } from '../../environments/environment'; // Asegurate de que la ruta a tus environments sea la correcta
 
 export const config = createConfig({
   chains: [sepolia],
   connectors: [
-    injected(),
+    walletConnect({
+      projectId: environment.reownProjectId,
+      showQrModal: false // Lo dejamos en false porque AppKit (Web3Modal) maneja su propia interfaz visual
+    }),
   ],
   transports: {
     [sepolia.id]: http(),
