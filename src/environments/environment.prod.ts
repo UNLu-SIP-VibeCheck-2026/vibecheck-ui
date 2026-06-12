2c32974683eccc149456b722549dd2cbe9b5f460
Nicolas Huici <111755754+nicodigo@users.noreply.github.com>
2026-06-12 13:26:21 -0300

    cansadisimo del error

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "https://vibecheck.team/api",
-  backendUrl: "https://vibecheck.team",
+  apiBaseUrl: "https://api.vibecheck.team/api",
+  backendUrl: "https://api.vibecheck.team",
   reownProjectId: "96f27291155de3fe8316b1cea2086922",
 };
