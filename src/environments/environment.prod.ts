b95426ce680ea4fedf8b65101d407cd47436c47e
Nicolas Huici <111755754+nicodigo@users.noreply.github.com>
2026-06-12 13:13:30 -0300

    cambio las url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "https://vibecheck-core.up.railway.app/api",
-  backendUrl: "https://vibecheck-core.up.railway.app",
+  apiBaseUrl: "https://vibecheck.team/api",
+  backendUrl: "https://vibecheck.team",
   reownProjectId: "96f27291155de3fe8316b1cea2086922",
 };
